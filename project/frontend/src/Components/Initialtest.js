c7d0e54da05b07a65dc37da0177b1374e2dd42d1
Abanob21 <wahba_abanob@yahoo.com>
2022-04-14 23:03:05 -0700

    editing Initialtest

--- project/frontend/src/Components/Initialtest.js
+++ project/frontend/src/Components/Initialtest.js
@@ -1,117 +1,123 @@
 //ES7+ React/Redux ... Search "React" in VSCode Extensions -> Type rafce in blank js file
 import axios from 'axios';
 import React, { useState, useEffect, Component } from 'react';
-import './Initialtest.css'
+import './Initialtest.css';
+import { searchName } from "../Middleware/api";
 
-const Initialtest = () => {
-    /*
-        export default class Search extends Component {
-            constructor() {
-                super()
-                this.state = {
-                    query: '',
-                    result: [],
-                    filteredResult: []
-                };
-            }
-        } */
-    const [data, setData] = useState({
-        response: '',
-        post: '',
-        responseToPost: ''
-    });
-
-    useEffect(() => {
-        callApi()
-    }, []);
-
-    const callApi = async () => {
-        return axios.get('/test/get')
-            .then(resAxios => {
-                setData({ ...data, "response": resAxios.data.express })
-                console.log("MESSAGE: " + resAxios.data.express)
-            })
-            .catch(err => {
-                alert("ERROR:" + err)
-            })
+class Initialtest extends Component {
+    state = {
+        response: "",
+        post: "",
+        responseToPost: "",
+        filters: ["State", "City", "County"],
+        choice: 0,
     };
 
+    /*when component first mounts onto website, run the following*/
+    componentDidMount() {
+        this.backendCallAPI()
+            .then((res) => this.setState({ response: res.express }))
+            .catch((err) => console.log(err));
+    }
 
-    const handleSubmit = async e => {
+    backendCallAPI = async() => {
+        return axios
+            .get("/test/get")
+            .then((res) => {
+                this.res = res.data;
+                if (res.status !== 200) throw Error(res.message);
+                return res.data;
+            })
+            .catch((err) => {
+                //console.log("HII: " + err)
+                alert("ERROR:" + err);
+            });
+    };
+
+    handleSubmit = async(e) => {
         e.preventDefault();
-        let res = { post: data.post }
-        console.log("Send: " + res)
-        axios.post('/test/post', res)
-            .then(resAxios => {
-                setData({ ...data, "responseToPost": resAxios.data })
-                console.log("Response: " + resAxios.data)
-            })
+        const ps = {
+            name: this.state.post,
+            choice: this.state.choice,
+        };
+        const response = await searchName(ps);
+        console.log(response.data);
+
+        // this.setState({ responseToPost: response.data });
     };
 
-    const [inputVal, setInputVal] = useState("");
-    const [info, setInfo] = useState({});
-    const [resValue, setResValue] = useState("");
+    //const searchSubmit = async (e) => {
+    // read ALL the data when searching for a single element
+    //  e.preventDefault()
+    // setResValue("")
+    //axios.get('test/get')
+    //  .then(res => {
+    //    console.log(res.data)
+    //  setInfo(res.data) //makes data accessible outside of scope
+    // Object.entries(info).map(([key, value]) => {
+    //   if (key.toLowerCase() == inputVal.toLowerCase()) {
+    //     setResValue(value)
+    //}
+    //console.log(value)
+    //})
+    //})
+    // .catch(err => {
+    //   alert(err)
+    //})
 
-    const handleTyping = (e) => {
-        setInputVal(e.target.value)
-        console.log(inputVal)
+    //}
+    render() {
+        return ( <
+            div className = "Initialtest" >
+            <
+            header className = "header" >
+            <
+            p >
+            CS180 Lab Connection Test <
+            /p> <
+            /header> <
+            p >
+            <
+            em > Server POST Message: < /em> {this.state.response} <
+            /p> <
+            form onSubmit = { this.handleSubmit } >
+            <
+            p >
+            <
+            strong > Post to Server: < /strong> <
+            /p> <
+            input type = "text"
+            value = { this.state.post }
+            onChange = {
+                (e) => this.setState({ post: e.target.value }) }
+            /> <
+            select value = { this.state.choice }
+            onChange = {
+                (e) => {
+                    this.setState({ choice: e.target.value });
+                }
+            } >
+            <
+            option key = { "empty" }
+            value = "" >
+            Please Select a Filter <
+            /option> {
+                this.state.filters.map((name, index) => ( <
+                    option key = { index }
+                    value = { index } > { name } <
+                    /option>
+                ))
+            } <
+            /select> <
+            button type = "submit" > Submit < /button> <
+            /form> <
+            p >
+            <
+            em > Server GET Message: < /em> {this.state.responseToPost} <
+            /p> <
+            /div>
+        );
     }
-
-    const searchSubmit = async (e) => {
-        // read ALL the data when searching for a single element
-        e.preventDefault()
-        setResValue("")
-        axios.get('test/get')
-            .then(res => {
-                console.log(res.data)
-                setInfo(res.data) //makes data accessible outside of scope
-                Object.entries(info).map(([key, value]) => {
-                    if (key.toLowerCase() == inputVal.toLowerCase()) {
-                        setResValue(value)
-                    }
-                    console.log(value)
-                })
-            })
-            .catch(err => {
-                alert(err)
-            })
-
-    }
-
-    return (
-        <div className="Initialtest">
-            <header className="header">
-                <p>
-                    CS180 Lab Connection Test
-                </p>
-            </header>
-            {/* <div className="test">
-            <p>Server POST Message: {data.response}</p>
-            <form onSubmit={handleSubmit}>
-                <p>
-                    <strong>Post to Server:</strong>
-                </p>
-                <input
-                    type="text"
-                    value={data.post} //<---? can work wthout || creates value so it can be updated
-                    //onChange = everytime add/remove a character
-                    //this.setState = setValue
-                    onChange={e => setData({ ...data, "post": e.target.value })}
-                />
-                <button type="submit">Submit</button>
-            </form>
-            <p>{data.responseToPost}</p>
-        </div> */}
-            <div className="search">
-                <form onSubmit={searchSubmit}>
-                    <p>Search</p>
-                    <input type="text" value={inputVal} onChange={handleTyping} />
-                    <button type="submit">Search</button>
-                    <p>{resValue}</p>
-                </form>
-            </div>
-        </div>
-    );
 }
 
 export default Initialtest
