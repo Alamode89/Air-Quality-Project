9e979cd988c3872b2df049c093a636ec16953261
Abanob21 <wahba_abanob@yahoo.com>
2022-04-21 19:03:02 -0700

    Adding import.

--- project/frontend/src/Components/AirQuality.js
+++ project/frontend/src/Components/AirQuality.js
@@ -4,108 +4,142 @@ import React, { useState, useEffect, Component } from 'react';
 import './AirQuality.css'
 
 const AirQuality = () => {
-    //------------------------Testing----------------------------------
-    const [data, setData] = useState({
-        response: '',
-        post: '',
-        responseToPost: ''
-    });
+        const [data, setData] = useState({
+            response: "",
+            post: "",
+            responseToPost: "",
+        });
+        //------------------------import----------------------------------//
+        const [filename, setfileaname] = useState("");
+        const [error, setError] = useState("");
+        const [rows, setRows] = useState([]);
+        const [columns, setColumns] = useState([]);
+        const [isLoading, setLoading] = useState(false);
+        const [filenames, setfilenames] = useState(["final_data"]);
 
-    useEffect(() => {
-        callApi()
-    }, []);
+        //this function is to send filename to server and fetch the data of that file
+        //after fetching we are showing that data in the form of table
+        const importData = () => {
+            //called an API with filename in body
+            if (!filename) {
+                // if we donot have a file name we are setting error here
+                setError("Please Enter filename");
+            } else {
+                //   in start we have made loading true
+                setLoading(true);
+                //   calling an API with file name
+                axios.post("/api/import/csv", { filename }).then((resAxios) => {
+                    let obj = resAxios.data.result[0];
+                    // to get column names
+                    const keys = Object.keys(obj);
+                    setColumns(obj);
+                    // to remove column name form table data
+                    resAxios.data.result.shift();
+                    // saving data in state
+                    setRows(resAxios.data.result);
+                    // making loading false
+                    setLoading(false);
+                });
+            }
+        };
+        //------------------------Testing----------------------------------
 
-    const callApi = async () => {
-        return axios.get('/test/get')
-            .then(resAxios => {
-                setData({ ...data, "response": resAxios.data.express })
-                console.log("MESSAGE: " + resAxios.data.express)
-            })
-            .catch(err => {
-                alert("ERROR:" + err)
-            })
-    };
+        useEffect(() => {
+            callApi()
+        }, []);
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        let res = { post: data.post }
-        console.log("Send: " + res)
-        axios.post('/test/post', res)
-            .then(resAxios => {
-                setData({ ...data, "responseToPost": resAxios.data })
-                console.log("Response: " + resAxios.data)
-            })
-    };
-
-    //------------------------Input----------------------------------
-
-    const [inputVal, setInputVal] = useState("");
-
-    const handleTyping = (e) => {
-        setInputVal(e.target.value)
-        console.log(inputVal)
-    }
-
-    //------------------------Search----------------------------------
-
-    const [search, setSearch] = useState({
-        latitude: '',
-        longitude: '',
-        parameter: '',
-        metric: '',
-        year: '',
-        observation_cnt: '',
-        arithmetic_mean: '',
-        arithmetic_stdDev: '',
-        county_name: '',
-        state_name: '',
-        city_name: ''
-    });
-
-    const searchSubmit = async (e) => {
-        // read ALL the data when searching for a single element
-        e.preventDefault()
-        //setResValue("")
-        let incoming = { city: inputVal }
-        console.log("Our input: " + incoming.city)
-        axios.post('/api/search', incoming)
-            .then(resAxios => {
-                console.log("Incoming Data: " + JSON.stringify(resAxios.data))
-                setSearch({ ...search, 
-                    "latitude": resAxios.data.latitude,
-                    "longitude": resAxios.data.longitude,
-                    "parameter": resAxios.data.parameter_name,
-                    "metric": resAxios.data.metric_used,
-                    "year": resAxios.data.year,
-                    "observation_cnt": resAxios.data.observation_count,
-                    "arithmetic_mean": resAxios.data.arithmetic_mean,
-                    "arithmetic_stdDev": resAxios.data.arithmetic_standard_dev,
-                    "county_name": resAxios.data.county_name,
-                    "state_name": resAxios.data.state_name,
-                    "city_name": resAxios.data.city_name
+        const callApi = async() => {
+            return axios.get('/test/get')
+                .then(resAxios => {
+                    setData({...data, "response": resAxios.data.express })
+                    console.log("MESSAGE: " + resAxios.data.express)
                 })
-            })
-            .catch(err => {
-                alert("ERROR:" + err)
-            })
+                .catch(err => {
+                    alert("ERROR:" + err)
+                })
+        };
 
-    }
+        const handleSubmit = async e => {
+            e.preventDefault();
+            let res = { post: data.post }
+            console.log("Send: " + res)
+            axios.post('/test/post', res)
+                .then(resAxios => {
+                    setData({...data, "responseToPost": resAxios.data })
+                    console.log("Response: " + resAxios.data)
+                })
+        };
 
-    const deleteSubmit = async (e) => {
-        // read all the data when searching for single element
-        e.preventDefault()
-        //setResValue("")
-        axios.get('/api/delete')
-            .then(resAxios => {
-                console.log("Status: " + JSON.stringify(resAxios.data.status))
-            })
-            .catch(err => {
-                alert("ERROR:" + err)
-            })
-    }
+        //------------------------Input----------------------------------
 
-    //------------------------HTML----------------------------------
-    return (
+        const [inputVal, setInputVal] = useState("");
+
+        const handleTyping = (e) => {
+            setInputVal(e.target.value)
+            console.log(inputVal)
+        }
+
+        //------------------------Search----------------------------------
+
+        const [search, setSearch] = useState({
+            latitude: '',
+            longitude: '',
+            parameter: '',
+            metric: '',
+            year: '',
+            observation_cnt: '',
+            arithmetic_mean: '',
+            arithmetic_stdDev: '',
+            county_name: '',
+            state_name: '',
+            city_name: ''
+        });
+
+        const searchSubmit = async(e) => {
+            // read ALL the data when searching for a single element
+            e.preventDefault()
+                //setResValue("")
+            let incoming = { city: inputVal }
+            console.log("Our input: " + incoming.city)
+            axios.post('/api/search', incoming)
+                .then(resAxios => {
+                    console.log("Incoming Data: " + JSON.stringify(resAxios.data))
+                    setSearch({...search,
+                        "latitude": resAxios.data.latitude,
+                        "longitude": resAxios.data.longitude,
+                        "parameter": resAxios.data.parameter_name,
+                        "metric": resAxios.data.metric_used,
+                        "year": resAxios.data.year,
+                        "observation_cnt": resAxios.data.observation_count,
+                        "arithmetic_mean": resAxios.data.arithmetic_mean,
+                        "arithmetic_stdDev": resAxios.data.arithmetic_standard_dev,
+                        "county_name": resAxios.data.county_name,
+                        "state_name": resAxios.data.state_name,
+                        "city_name": resAxios.data.city_name
+                    })
+                })
+                .catch(err => {
+                    alert("ERROR:" + err)
+                })
+
+        }
+
+        const deleteSubmit = async(e) => {
+            // read all the data when searching for single element
+            e.preventDefault()
+                //setResValue("")
+            axios.get('/api/delete')
+                .then(resAxios => {
+                    console.log("Status: " + JSON.stringify(resAxios.data.status))
+                })
+                .catch(err => {
+                    alert("ERROR:" + err)
+                })
+        }
+
+        //------------------------HTML----------------------------------
+         return (
+        <>
         <div className="AirQuality">
             <header className="header">
                 <p>
@@ -138,63 +172,112 @@ const AirQuality = () => {
                 </form>
             </div>
         </div>
-    );
-}
+         {/* //from here import will start  */}
+      <div className="Initialtest">
+        <h1>Import</h1>
+        <div className="import-div">
+          <input
+            type="text"
+            value={filename}
+            onChange={(e) => {
+              setError("");
+              setfileaname(e.target.value);
+            }}
+          />
+          <button onClick={() => importData()}>Search</button>
+        </div>
+        {/* to show error if we note enter filename */}
+        <p style={{ textALign: "center", color: "red" }}>{error}</p>
+        {/* this is to show available filenames  */}
+        <div className="import-div">
+          <p style={{ textALign: "left" }}>
+            Enter filename from these available files:{" "}
+            {filenames.map((fm) => (
+              <li>{fm + " "}</li>
+            ))}
+          </p>
+        </div>
+
+        {/* when we call API it will take time so we are shoiwng loading here  */}
+
+        <div style={{ marginTop: "30px" }}>
+          {rows.length ? (
+            //   this is table to show data
+
+            <table>
+              <tr>
+                {columns.map((col) => (col ? <th>{col}</th> : <th>id</th>))}
+              </tr>
+              {rows.map((row) => (
+                <tr key={row.name}>
+                  {row.map((col) => (col ? <td>{col}</td> : null))}
+                </tr>
+              ))}
+            </table>
+          ) : (
+            //   if do not have any data we will show this message instead of empty table
+            <h4>Please enter filename to import that file...</h4>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default AirQuality
 
-/* Notes:
-    
-export default class Search extends Component {
-    constructor() {
-        super()
-        this.state = {
-            query: '',
-            result: [],
-            filteredResult: []
-        };
-    }
-} 
+                    /* Notes:
+                        
+                    export default class Search extends Component {
+                        constructor() {
+                            super()
+                            this.state = {
+                                query: '',
+                                result: [],
+                                filteredResult: []
+                            };
+                        }
+                    } 
 
-<div className="test">
-    <p>Server POST Message: {data.response}</p>
-    <form onSubmit={handleSubmit}>
-        <p>
-            <strong>Post to Server:</strong>
-        </p>
-        <input
-            type="text"
-            value={data.post} //<---? can work wthout || creates value so it can be updated
-            //onChange = everytime add/remove a character
-            //this.setState = setValue
-            onChange={e => setData({ ...data, "post": e.target.value })}
-        />
-        <button type="submit">Submit</button>
-    </form>
-    <p>{data.responseToPost}</p>
-</div> 
+                    <div className="test">
+                        <p>Server POST Message: {data.response}</p>
+                        <form onSubmit={handleSubmit}>
+                            <p>
+                                <strong>Post to Server:</strong>
+                            </p>
+                            <input
+                                type="text"
+                                value={data.post} //<---? can work wthout || creates value so it can be updated
+                                //onChange = everytime add/remove a character
+                                //this.setState = setValue
+                                onChange={e => setData({ ...data, "post": e.target.value })}
+                            />
+                            <button type="submit">Submit</button>
+                        </form>
+                        <p>{data.responseToPost}</p>
+                    </div> 
 
-axios.get('test/iloveithere')
-    .then(res => {
-        console.log(res.data) //our array from backend
-        setInfo(res.data) //makes data accessible outside of scope
-        Object.entries(info).map(([key, value]) => {
-            if (key.toLowerCase() == inputVal.toLowerCase()) {
-                setResValue(value)
-            }
-            console.log(value)
-        })
-    })
-    .catch(err => {
-        alert(err)
-    })
+                    axios.get('test/iloveithere')
+                        .then(res => {
+                            console.log(res.data) //our array from backend
+                            setInfo(res.data) //makes data accessible outside of scope
+                            Object.entries(info).map(([key, value]) => {
+                                if (key.toLowerCase() == inputVal.toLowerCase()) {
+                                    setResValue(value)
+                                }
+                                console.log(value)
+                            })
+                        })
+                        .catch(err => {
+                            alert(err)
+                        })
 
 
-var whatWeGot = ""
-            whatWeGot += "Latitude: " + JSON.stringify(resAxios.data.latitude)
-            whatWeGot += "\nLongitude: " + JSON.stringify(resAxios.data.longitude)
-            whatWeGot += "\nParameter: " + JSON.stringify(resAxios.data.parameter_name)
-            whatWeGot += "\nyear: " + JSON.stringify(resAxios.data.year)
+                    var whatWeGot = ""
+                                whatWeGot += "Latitude: " + JSON.stringify(resAxios.data.latitude)
+                                whatWeGot += "\nLongitude: " + JSON.stringify(resAxios.data.longitude)
+                                whatWeGot += "\nParameter: " + JSON.stringify(resAxios.data.parameter_name)
+                                whatWeGot += "\nyear: " + JSON.stringify(resAxios.data.year)
 
-            setResValue(whatWeGot)
-*/
+                                setResValue(whatWeGot)
+                    */
--- project/server.js
+++ project/server.js
@@ -1,12 +1,15 @@
 //Packages: express, body-parser, ...
-const parse = require("./utility/parseCSV");
+const parsecsv = require("./utility/parseCSV");
 const search = require("./utility/search");
 const deleteEntry = require("./utility/deleteEntry");
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var fs = require("fs");
+const path = require("path");
 const port = process.env.PORT || 1337;
+const { parse } = require("fast-csv");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -32,12 +35,12 @@ app.post('/test/post', (req, res) => {
 // CSV Parse
 console.time("CSVParseTime");
 let rows = [];
-rows = parse.readCSVFile("final_data.csv");
+rows = parsecsv.readCSVFile("final_data.csv");
 console.log("Parsed " + rows.length + " rows.");
 console.timeEnd("CSVParseTime");
 
 // parser bug: last row is always blank, needs actual fix
-rows.splice(rows.length-1, 1);
+rows.splice(rows.length - 1, 1);
 /*
 console.log(rows[rows.length - 5])
 console.log(rows[rows.length - 4])
@@ -49,27 +52,45 @@ console.log(rows[rows.length - 1])
 
 // delete a dataset entry
 app.get('/api/delete', (req, res) => {
-  // userHasSearched will be set to false again once entry has been deleted
-  // requires user to search again in order to either delete or update entries
-  userHasSearched = deleteEntry.deleteEntry(searchStatus, userHasSearched, rows);
-  res.send({"status": "successfully deleted"});
+    // userHasSearched will be set to false again once entry has been deleted
+    // requires user to search again in order to either delete or update entries
+    userHasSearched = deleteEntry.deleteEntry(searchStatus, userHasSearched, rows);
+    res.send({ "status": "successfully deleted" });
 })
 
 app.post('/api/search', (req, res) => {
-  console.log(req.body.city);
-  searchStatus = search.searchCity(req.body.city, rows);
-  if (Object.keys(searchStatus).length != 0) {
-    console.log("Search Success" + searchStatus);
-    // user has now used search, may enter the update and delete functions
-    userHasSearched = true;
-    res.send (
-      searchStatus
-    )
-  }
-  else {
-    console.log("Search: Failed");
-  }
+    console.log(req.body.city);
+    searchStatus = search.searchCity(req.body.city, rows);
+    if (Object.keys(searchStatus).length != 0) {
+        console.log("Search Success" + searchStatus);
+        // user has now used search, may enter the update and delete functions
+        userHasSearched = true;
+        res.send(
+            searchStatus
+        )
+    } else {
+        console.log("Search: Failed");
+    }
 
+});
+//API to fetch filename and return file data of that scpecific file
+app.post("/api/import/csv", async(req, res) => {
+    console.log(req.body);
+    const result = [];
+    await fs
+        .createReadStream(path.resolve(`./${req.body.filename}.csv`))
+        .pipe(parse({ maxRows: 50 }))
+        .on("error", (error) => console.error({ error }))
+        .on("data", (row) => {
+            console.log({ row });
+            result.push(row);
+        })
+        .on("end", (rowCount) => {
+            console.timeEnd("Timer1");
+            res.send({ result });
+
+            console.log(`Parsed ${rowCount} rows`);
+        });
 });
 
 
