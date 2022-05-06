dcc4db59f070a9e9d1fde27f277ec7e3aabce2bc
Alamode89 <andre@Andres-MacBook-Pro.local>
2022-05-06 00:57:42 -0700

    Redesign Version 1

--- project/frontend/src/Components/Layer/Navbar.js
+++ project/frontend/src/Components/Layer/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Navbar.css'
-
+import navImage from '../../navBarImage.png';
 
 const Navbar = () => {
   return (
-    <div className="Navbar">Navbar</div>
+    <div className="Navbar">
+        <img className="navImage" src={navImage} alt="navImage" />
+      <div className="tabs">
+        <a href='/' class="nav">Home</a>
+        <a href='/graph' class="nav">Data</a>
+        <a href='/test/graph' class="nav">Graph</a>
+      </div>
+    </div>
   )
 }
 
